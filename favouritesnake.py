1e42215ed868b6422efadefd3e3afbe974ca266c
alicekatherine <aliceksmith95@gmail.com>
2018-03-21 21:01:43 +0000

    Update

--- favouritesnake.py
+++ favouritesnake.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key = 'x0jGy3Hx0aBDSvZK7kBkS7y6B'
-consumer_secret = 'zVOkrQRwZjDdnVGikJNOld06DBpBVdDHahE2AkGVynJQemCWuT'
-access_token = '971448341635784707-9oZobc20SUG6TWJmN2cCeEwb4x4jr2D'
-access_token_secret = 'EgDKzGlhLVWapXzEk2zCWdapb9qvqVQXjTSNY3rZssg2y'
+consumer_key = 'xxxxx'
+consumer_secret = 'xxxxx'
+access_token = 'xxxxx'
+access_token_secret = 'xxxxx'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
